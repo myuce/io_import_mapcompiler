916919c47001e73548b847ac78177c58f96a4eec
Mehmet <myuce153@hotmail.com>
2023-05-08 01:25:09 +0300

    Add default material & update lightmap data creation

--- builders/MaterialBuilder.py
+++ builders/MaterialBuilder.py
@@ -2,12 +2,44 @@ import bpy
 import os
 from ..qmap.Map import Map
 from ..func.Helpers import newPath
+from .LmapBuilder import CreateLightmapImage
 
-def BuildMaterials(mapData: Map, game_path: str):
-    # Create a new blank image with 1024x1024 pixels
-    lightmap_image = bpy.data.images.new(name="lightmap", width=1024, height=1024, alpha=False)
-    pixels = [1.0] * (1024 * 1024 * 4) # Fill with white color
-    lightmap_image.pixels = pixels
+# material with checker texture used by objects with no material to be found
+def CreateDefaultMaterial():
+        # Create a new material and set it up
+        material = bpy.data.materials.new(name="404")
+        material.use_nodes = True
+        nodes = material.node_tree.nodes
+
+        for node in nodes:
+            nodes.remove(node)
+
+        # Create a texture node for the diffuse image and set its image to the file we found
+        tex_node = nodes.new(type="ShaderNodeTexChecker")
+
+        # Create a diffuse node and an output node
+        diffuse_node = nodes.new(type="ShaderNodeBsdfDiffuse")
+        output_node = nodes.new(type="ShaderNodeOutputMaterial")
+        links = material.node_tree.links
+        links.new(tex_node.outputs["Color"], diffuse_node.inputs["Color"])
+        links.new(diffuse_node.outputs["BSDF"], output_node.inputs["Surface"])
+
+        # Create a texture node for the lightmap and set its image to the blank image we created earlier
+        lightmap_node = nodes.new(type="ShaderNodeTexImage")
+        lightmap_node.image = bpy.data.images["LightmapImage"]
+
+        # create a uv map node and set its uv layer
+        uv_node = nodes.new(type="ShaderNodeUVMap")
+        uv_node.uv_map = "LightmapUV"
+
+        # link the lightmap image and the uv node
+        links.new(uv_node.outputs["UV"], lightmap_node.inputs["Vector"])
+
+        # set the lightmap image as the active node
+        nodes.active = lightmap_node
+
+def BuildMaterials(mapData: Map, game_path: str, lightmapImageSize=(1024, 1024)):
+    lightmapImage = CreateLightmapImage(*lightmapImageSize)
 
     extensions = ["tga", "jpg", "png"]
 
@@ -36,17 +68,37 @@ def BuildMaterials(mapData: Map, game_path: str):
         tex_node = nodes.new(type="ShaderNodeTexImage")
         tex_node.image = bpy.data.images.load(file)
 
-        # Create a texture node for the lightmap and set its image to the blank image we created earlier
-        lightmap_node = nodes.new(type="ShaderNodeTexImage")
-        lightmap_node.image = lightmap_image
-
-        # Create a mix shader node to combine the diffuse texture and lightmap
-        mix_shader_node = nodes.new(type="ShaderNodeMixShader")
-        # mix_shader_node.inputs[0].default_value = 0.5 # Factor
-
         # Create a diffuse node and an output node
         diffuse_node = nodes.new(type="ShaderNodeBsdfDiffuse")
         output_node = nodes.new(type="ShaderNodeOutputMaterial")
         links = material.node_tree.links
         links.new(tex_node.outputs["Color"], diffuse_node.inputs["Color"])
         links.new(diffuse_node.outputs["BSDF"], output_node.inputs["Surface"])
+
+        # Create a texture node for the lightmap and set its image to the blank image we created earlier
+        lmap_img_node = nodes.new(type="ShaderNodeTexImage")
+        lmap_img_node.image = lightmapImage
+
+        # create a uv map node and set its uv layer
+        uv_node = nodes.new(type="ShaderNodeUVMap")
+        uv_node.uv_map = "LightmapUV"
+
+        # link the lightmap image and the uv node
+        links.new(uv_node.outputs["UV"], lmap_img_node.inputs["Vector"])
+
+        # set the lightmap image as the active node
+        nodes.active = lmap_img_node
+
+        # create a shader for lightmap
+        lmap_diffuse_node = nodes.new(type="ShaderNodeBsdfDiffuse")
+        lmap_output_node = nodes.new(type="ShaderNodeOutputMaterial")
+        links.new(lmap_img_node.outputs["Color"], lmap_diffuse_node.inputs["Color"])
+        links.new(lmap_diffuse_node.outputs["BSDF"], lmap_output_node.inputs["Surface"])
+
+        # create mix shader to mix diffuse and lightmap
+        mix_node = nodes.new(type="ShaderNodeMixShader")
+        mix_output_node = nodes.new(type="ShaderNodeOutputMaterial")
+
+        links.new(mix_output_node.inputs["Surface"], mix_node.outputs["Shader"])
+        links.new(mix_node.inputs[1], diffuse_node.outputs["BSDF"])
+        links.new(mix_node.inputs[2], lmap_diffuse_node.outputs["BSDF"])
